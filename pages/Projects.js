91e133560e45b7eddb56d3091beb9fb8d436b3db
Goran-Sacic <goran.sacic@gmail.com>
2023-10-17 18:10:57 +0200

    n0.1

--- pages/Projects.js
+++ pages/Projects.js
@@ -7,7 +7,7 @@ import MyPortfolio from "../images/MyPortfolio.png";
 import Netlify from "../logos/Netlify.png";
 import GitHubLogoWide from "../logos/GitHub logo wide.png";
 import Playart from "../images/Playart.png";
-import kodimon from "../images/PokemonBattle.jpg;";
+import Pokemon from "../images/PokemonBattle.png";
 /* import PlayartLogo from "../logos/PlayArt.jpg"; */
 
 import ProjectDetails from "../components/ProjectDetails";
@@ -93,7 +93,7 @@ const myProjects = [
   {
     id: "Pokemon",
     name: "Pokemon",
-    image: kodimon,
+    image: Pokemon,
     alt: "pokemon battle aplikacija",
     description: "Pokemon battle - WIP",
     link: "https://kodi3.vercel.app/HomePage",
